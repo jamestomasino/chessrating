0088de91af356ca2598616afcb6cd398099cfd18
James Tomasino <james@tomasino.org>
2013-07-11 19:41:13 -0400

    styling

--- sjs/main.js
+++ sjs/main.js
@@ -1,27 +1,60 @@
-/*
- * New Rating = Old Rating + K * (Score - Win Expectancy)
- * K is a constant (32 for 0-2099, 24 for 2100-2399, 16 for 2400 and above)
- * Score is 1 for a win, 0.5 for a draw and 0 for a loss.
- * The Win Expectancy is calculated using the following formula :
- * Win Expectancy = 1 / (10^((OpponentRating-YourRating)/400)+1)
- */
+//= require lib/jquery-1.9.1.min
+//= require lib/jquery.numeric
+//= require lib/fancyInput
 
-rating_1 = 1200;
-rating_2 = 1200;
+$('input[type=text]').numeric();
+$('.fancy_input :input').fancyInput();
 
-K = 32;
+var result_1 = 0,
+	result_2 = 0,
+	rating_1 = 0,
+	rating_2 = 0,
+	K_1 = 0,
+	K_2 = 0,
+	diff_1 = 0,
+	diff_2 = 0,
+	win_expectancy_1 = 0
+	win_expectancy_2 = 0
+	new_rating_1 = 0,
+	new_rating_2 = 0;
 
-result_1 = 1; // 1 = win, 0.5 = draw, 0 = loss
-result_2 = (result_1 === 1) ? 0 : (result_1 === 0) ? 1 : 0.5;
+function calculate() {
 
-diff_1 = ((rating_2 - rating_1) / 400) + 1;
-diff_2 = ((rating_1 - rating_2) / 400) + 1;
+	var winner = document.getElementsByName('winner');
+	for (var i = 0; i < winner.length; ++i ){
+		if (winner[i].checked) {
+			result_1 = (winner[i].value == "black") ? 0 : (winner[i].value == "white") ? 1 : 0.5;
+			break;
+		}
+	}
 
-win_expectancy_1 = 1 / ( Math.pow( 10, diff_1 ) );
-win_expectancy_2 = 1 / ( Math.pow( 10, diff_2 ) );
+	rating_1 = parseInt(document.getElementById('player_1_rating').value, 10);
+	rating_2 = parseInt(document.getElementById('player_2_rating').value, 10);
 
-new_rating_1 = Math.round(rating_1 + K * (result_1 - win_expectancy_1));
-new_rating_2 = Math.round(rating_2 + K * (result_2 - win_expectancy_2));
+	if ( isNaN(rating_1) || rating_1 === undefined ) rating_1 = 1200;
+	if ( isNaN(rating_2) || rating_2 === undefined ) rating_2 = 1200;
 
-console.log (new_rating_1);
-console.log (new_rating_2);
+	document.getElementById('player_1_rating').value = String(rating_1);
+	document.getElementById('player_2_rating').value = String(rating_2);
+
+	K_1 = (rating_1 < 2100) ? 32: (rating_1 < 2400) ? 24 : 16;
+	K_2 = (rating_2 < 2100) ? 32: (rating_2 < 2400) ? 24 : 16;
+
+	//result_1 = 1; // 1 = win, 0.5 = draw, 0 = loss
+	result_2 = (result_1 === 1) ? 0 : (result_1 === 0) ? 1 : 0.5;
+
+	diff_1 = ((rating_2 - rating_1) / 400) + 1;
+	diff_2 = ((rating_1 - rating_2) / 400) + 1;
+
+	win_expectancy_1 = 1 / ( Math.pow( 10, diff_1 ) );
+	win_expectancy_2 = 1 / ( Math.pow( 10, diff_2 ) );
+
+	new_rating_1 = Math.max( 100, Math.round(rating_1 + K_1 * (result_1 - win_expectancy_1)));
+	new_rating_2 = Math.max( 100, Math.round(rating_2 + K_2 * (result_2 - win_expectancy_2)));
+
+	document.getElementById('player_1_new_rating').innerHTML = String(new_rating_1);
+	document.getElementById('player_2_new_rating').innerHTML = String(new_rating_2);
+
+}
+
+
