a70585341cfa032232edd23415f139dd3cb9dafd
James Tomasino <james@tomasino.org>
2013-07-11 17:49:13 -0400

    added basic chess rating math

--- sjs/main.js
+++ sjs/main.js
@@ -1,2 +1,27 @@
-//= require lib/jquery-1.8.2
-//= require lib/l10n
+/*
+ * New Rating = Old Rating + K * (Score - Win Expectancy)
+ * K is a constant (32 for 0-2099, 24 for 2100-2399, 16 for 2400 and above)
+ * Score is 1 for a win, 0.5 for a draw and 0 for a loss.
+ * The Win Expectancy is calculated using the following formula :
+ * Win Expectancy = 1 / (10^((OpponentRating-YourRating)/400)+1)
+ */
+
+rating_1 = 1200;
+rating_2 = 1200;
+
+K = 32;
+
+result_1 = 1; // 1 = win, 0.5 = draw, 0 = loss
+result_2 = (result_1 === 1) ? 0 : (result_1 === 0) ? 1 : 0.5;
+
+diff_1 = ((rating_2 - rating_1) / 400) + 1;
+diff_2 = ((rating_1 - rating_2) / 400) + 1;
+
+win_expectancy_1 = 1 / ( Math.pow( 10, diff_1 ) );
+win_expectancy_2 = 1 / ( Math.pow( 10, diff_2 ) );
+
+new_rating_1 = Math.round(rating_1 + K * (result_1 - win_expectancy_1));
+new_rating_2 = Math.round(rating_2 + K * (result_2 - win_expectancy_2));
+
+console.log (new_rating_1);
+console.log (new_rating_2);
